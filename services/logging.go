00921937d4d75ff9b80ef9e84ca8cf86be8ff9a1
Ernesto Alejo <ernestokarim@gmail.com>
2019-02-28 23:44:11 +0100

    services: Mapea los niveles de Logrus a niveles de Stackdriver al loguear.
    Change-Id: I32362c2ec9c77444251ed1e93eb263939ccb01d4

--- services/logging.go
+++ services/logging.go
@@ -4,6 +4,51 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type stackdriverFormatter struct {
+	log.JSONFormatter
+}
+
+func (f *stackdriverFormatter) Format(entry *log.Entry) ([]byte, error) {
+	switch entry.Level {
+	// EMERGENCY (800) One or more systems are unusable.
+	case log.PanicLevel:
+		entry.Data["severity"] = 800
+
+	// ALERT (700) A person must take an action immediately.
+	case log.FatalLevel:
+		entry.Data["severity"] = 700
+
+	// CRITICAL (600) Critical events cause more severe problems or outages.
+	// No equivalent in logrus.
+
+	// ERROR (500) Error events are likely to cause problems.
+	case log.ErrorLevel:
+		entry.Data["severity"] = 500
+
+	// WARNING (400) Warning events might cause problems.
+	case log.WarnLevel:
+		entry.Data["severity"] = 400
+
+	// NOTICE (300) Normal but significant events, such as start up, shut down, or a configuration change.
+	// No equivalent in logrus.
+
+	// INFO (200) Routine information, such as ongoing status or performance.
+	case log.InfoLevel:
+		entry.Data["severity"] = 200
+
+	// DEBUG (100) Debug or trace information.
+	case log.DebugLevel:
+		entry.Data["severity"] = 100
+
+	// DEFAULT (0) The log entry has no assigned severity level.
+	case log.TraceLevel:
+		entry.Data["severity"] = 0
+	}
+	entry.Data["severity"] = entry.Level.String()
+
+	return f.JSONFormatter.Format(entry)
+}
+
 func init() {
 	if IsLocal() {
 		log.SetFormatter(&log.TextFormatter{
@@ -11,6 +56,6 @@ func init() {
 		})
 		log.SetLevel(log.DebugLevel)
 	} else {
-		log.SetFormatter(new(log.JSONFormatter))
+		log.SetFormatter(new(stackdriverFormatter))
 	}
 }
