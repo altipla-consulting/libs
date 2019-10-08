5761c6940c8e6423df6003f5f436d47554c3b345
antonio <antonio.altipla@gmail.com>
2019-10-08 08:55:08 +0200

    templates: Añade método para formatear el dinero en la moneda dada.
    Change-Id: I5242139f1380699fc88a86c3bb118364b1ab01f5

--- templates/funcs/money.go
+++ templates/funcs/money.go
@@ -5,3 +5,7 @@ import "libs.altipla.consulting/money"
 func Price(value int32) string {
 	return money.NewFromCents(int64(value)).Format(2)
 }
+
+func Money(value int32, currency string) string {
+	return money.NewFromCents(int64(value)).Display(currency)
+}
