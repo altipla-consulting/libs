d8a7b313022b9ff1e726d9d808e5e559b03fb025
Ernesto Alejo <ernestokarim@gmail.com>
2020-02-16 02:40:46 +0100

    infra: Elimina algunos chequeos que ya realiza el linter por sí solo.
    Change-Id: I07add922f36a142a676f04e80df10de1a268e870

--- infra/lint-errors.sh
+++ infra/lint-errors.sh
@@ -2,33 +2,6 @@
 
 set -eu
 
-ERR=$(find . -type f -name '*.go' -not -path './vendor/*' -not -path './errors/*' -exec grep -Hn '"errors"' {} \;)
-if [[ $ERR ]]; then
-  echo "Files with errors import:"
-  echo "$ERR"
-  echo
-  echo " >>> check-errors failed"
-  exit 1
-fi
-
-ERR=$(find . -type f -name '*.go' -not -path './vendor/*' -exec grep -Hn '"github.com/juju/errors"' {} \;)
-if [[ $ERR ]]; then
-  echo "Files with github.com/juju/errors import:"
-  echo "$ERR"
-  echo
-  echo " >>> check-errors failed"
-  exit 1
-fi
-
-ERR=$(find . -type f -name '*.go' -not -path './vendor/*' -exec grep -Hn '"github.com/altipla-consulting/errors"' {} \;)
-if [[ $ERR ]]; then
-  echo "Files with github.com/altipla-consulting/errors import:"
-  echo "$ERR"
-  echo
-  echo " >>> check-errors failed"
-  exit 1
-fi
-
 ERR=$(find . -type f -name '*.go' -not -path './vendor/*' -not -path './errors/*' -not -name '*.pb.go' -exec grep -Hn 'fmt.Errorf' {} \;)
 if [[ $ERR ]]; then
   echo "Files with fmt.Errorf:"
