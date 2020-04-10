9a9be229e9eecead9a9d1213003bb1096fe76b56
Ernesto Alejo <ernestokarim@gmail.com>
2020-04-10 02:55:00 +0200

    env: Detecta ejecuciones en Jenkins.
    Change-Id: Ic0fa0e6fd00a56c4605f4ddd01c3bbaa7ff9c9d2

--- env/env.go
+++ env/env.go
@@ -10,6 +10,11 @@ func IsLocal() bool {
 	return Version() == ""
 }
 
+// IsJenkins detects if we are running as a step of a Jenkins build.
+func IsJenkins() bool {
+	return os.Getenv("BUILD_ID") != ""
+}
+
 // Version returns the environment variable VERSION. In development it should be empty.
 // In production it should be set accordingly; it may be for example the container hash.
 func Version() string {
