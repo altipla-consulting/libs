2a4cec0a309c7aba21b9b689d2fc607eb88944f1
Ernesto Alejo <ernestokarim@gmail.com>
2020-08-23 20:42:13 +0200

    env: Utiliza el hostname si el nombre de la app no está disponible.
    Change-Id: I7345d9c91136d4c39f82bd9fd5562dcddcc6b969

--- env/env.go
+++ env/env.go
@@ -15,12 +15,14 @@ func IsJenkins() bool {
 	return os.Getenv("BUILD_ID") != ""
 }
 
+// IsCloudRun detects if we are running inside a Cloud Run app.
 func IsCloudRun() bool {
 	return os.Getenv("K_CONFIGURATION") != ""
 }
 
-// Version returns the environment variable VERSION. In development it should be empty.
-// In production it should be set accordingly; it may be for example the container hash.
+// Version returns the environment variable K_REVISION or VERSION. In development
+// it should be empty. In production it should be set accordingly; it may be for
+// example the container hash.
 func Version() string {
 	if e := os.Getenv("K_REVISION"); e != "" {
 		return e
@@ -28,6 +30,15 @@ func Version() string {
 	return os.Getenv("VERSION")
 }
 
+// ServiceName returns the K_SERVICE environment variable or the hostname if not
+// available as it is common during development.
 func ServiceName() string {
-	return os.Getenv("K_SERVICE")
+	if v := os.Getenv("K_SERVICE"); v != "" {
+		return v
+	}
+	v, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return v
 }
