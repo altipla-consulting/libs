6e72bf39ce885d17d5ff9687067e690a441c070d
Ernesto Alejo <ernestokarim@gmail.com>
2021-10-12 16:52:40 +0200

    security: Añade utilidades para extraer la autenticación de una petición HTTP.
    Change-Id: Ie51fc2643d4c9bb74810144a9d33357cf4850c6e

--- security/read.go
+++ security/read.go
@@ -2,6 +2,7 @@ package security
 
 import (
 	"context"
+	"net/http"
 	"strings"
 
 	"google.golang.org/grpc/metadata"
@@ -11,6 +12,16 @@ func ReadServiceAuthorization(ctx context.Context) string {
 	md, _ := metadata.FromIncomingContext(ctx)
 	if auth := md.Get("authorization"); len(auth) > 0 {
 		parts := strings.SplitN(auth[0], " ", 2)
+		if len(parts) == 2 && parts[0] == "Bearer" {
+			return parts[1]
+		}
+	}
+	return ""
+}
+
+func ReadRequestAuthorization(r *http.Request) string {
+	if auth := r.Header.Get("authorization"); auth != "" {
+		parts := strings.SplitN(auth, " ", 2)
 		if len(parts) == 2 && parts[0] == "Bearer" {
 			return parts[1]
 		}
