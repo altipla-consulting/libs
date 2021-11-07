e3b64ac90646d18d5f4ae006379cdb3a9ac736ad
Ernesto Alejo <ernestokarim@gmail.com>
2021-11-07 20:33:39 +0100

    env: Soporta Azure Functions.
    Change-Id: Ia1225eddb952fc2e1537c6cae5fc30f8b37b481f

--- env/env.go
+++ env/env.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"io/ioutil"
 	"os"
 )
 
@@ -20,22 +21,46 @@ func IsCloudRun() bool {
 	return os.Getenv("K_CONFIGURATION") != ""
 }
 
-// Version returns the environment variable K_REVISION or VERSION. In development
-// it should be empty. In production it should be set accordingly; it may be for
-// example the container hash.
+// IsAzureFunction detects if we are running inside an Azure Function app.
+func IsAzureFunction() bool {
+	return os.Getenv("APPSETTING_WEBSITE_SITE_NAME") != ""
+}
+
+// Version returns the application version. In supported environment it may extract
+// the info from files or environment variables. Otherwise it will use the env variable
+// VERSION that should be set manually to the desired value.
 func Version() string {
+	// Cloud Run.
 	if e := os.Getenv("K_REVISION"); e != "" {
 		return e
 	}
+
+	// Azure Function has a Kubu file one level up of the working directory.
+	if IsAzureFunction() {
+		v, err := ioutil.ReadFile("../deployments/active")
+		if err != nil {
+      return string(v)
+		}
+	}
+
+	// Generic settings we have to set as custom options in each platform.
 	return os.Getenv("VERSION")
 }
 
-// ServiceName returns the K_SERVICE environment variable or the hostname if not
-// available as it is common during development.
+// ServiceName returns the version of the application from the environment-dependent
+// variables or the hostname if none is available.
 func ServiceName() string {
+	// Cloud Run.
 	if v := os.Getenv("K_SERVICE"); v != "" {
 		return v
 	}
+
+	// Azure Functions.
+	if v := os.Getenv("APPSETTING_WEBSITE_SITE_NAME"); v != "" {
+		return v
+	}
+
+	// Last option is a hostname which should always exist, though is not very useful usually.
 	v, err := os.Hostname()
 	if err != nil {
 		panic(err)
